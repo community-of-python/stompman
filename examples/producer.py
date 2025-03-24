c7d87218c3f828ab225b367544bcc2c4e98b234e
Lev Vereshchagin <mail@vrslev.com>
2025-03-24 21:00:34 +0300

    Adjust print statements to include noqa comment for T201

--- examples/producer.py
+++ examples/producer.py
@@ -8,16 +8,16 @@ server = stompman.ConnectionParameters(host="127.0.0.1", port=9000, login="admin
 async def main() -> None:
     async with stompman.Client([server]) as client:
         await client.send(b"Hi!", "DLQ")
-        print("Said hi")
+        print("Said hi")  # noqa: T201
 
         async with client.begin() as transaction:
             for index in range(5):
                 await transaction.send(b"Hi from transaction! " + str(index).encode(), "DLQ")
-                print(f"Said hi in transaction ({index})")
+                print(f"Said hi in transaction ({index})")  # noqa: T201
                 await asyncio.sleep(0.3)
 
         await client.send(b"Mu-ha-ha!", "DLQ")
-        print("Laughed evilly")
+        print("Laughed evilly")  # noqa: T201
 
 
 if __name__ == "__main__":
