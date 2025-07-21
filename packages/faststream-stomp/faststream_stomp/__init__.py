c41b72306b5f2c79a7189be2af8a54e4a0d5cc6f
Lev Vereshchagin <mail@vrslev.com>
2025-07-21 17:37:09 +0300

    Add import for StompPublishCommand in __init__.py

--- packages/faststream-stomp/faststream_stomp/__init__.py
+++ packages/faststream-stomp/faststream_stomp/__init__.py
@@ -1,5 +1,5 @@
 from faststream_stomp.broker import StompBroker
-from faststream_stomp.models import StompStreamMessage
+from faststream_stomp.models import StompPublishCommand, StompStreamMessage
 from faststream_stomp.publisher import StompPublisher
 from faststream_stomp.router import StompRoute, StompRoutePublisher, StompRouter
 from faststream_stomp.subscriber import StompSubscriber
