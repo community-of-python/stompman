eee331b874a7f0b8acb147651f657be46bc41b72
Lev Vereshchagin <mail@vrslev.com>
2026-03-10 16:21:56 +0300

    Register StompBroker for AsyncAPI UI

--- packages/faststream-stomp/faststream_stomp/__init__.py
+++ packages/faststream-stomp/faststream_stomp/__init__.py
@@ -16,3 +16,21 @@ __all__ = [
     "StompSubscriber",
     "TestStompBroker",
 ]
+
+try:  # noqa: RUF067
+    import functools
+    import typing
+
+    import faststream.asgi.factories.asyncapi.try_it_out
+    from faststream._internal.broker import BrokerUsecase
+    from faststream._internal.testing.broker import TestBroker
+
+    original_get_broker_registry = faststream.asgi.factories.asyncapi.try_it_out._get_broker_registry
+
+    @functools.lru_cache(maxsize=1)
+    def get_broker_registry() -> dict[type[BrokerUsecase[typing.Any, typing.Any]], type[TestBroker[typing.Any]]]:
+        return {**original_get_broker_registry(), StompBroker: TestStompBroker}
+
+    faststream.asgi.factories.asyncapi.try_it_out._get_broker_registry = get_broker_registry
+except Exception:  # noqa: BLE001, S110
+    pass
